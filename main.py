a15a876f65b5c4ac178a426350bf843c3406f48e
Jatin Wadhwa <jatinw21@gmail.com>
2019-07-25 17:12:51 +1000

    Arguments explained in main.py

--- main.py
+++ main.py
@@ -15,42 +15,57 @@ params['DataManagerParams'] = dict()
 params['ModelParams'] = dict()
 
 #  params of the algorithm
-# params['ModelParams']['numcontrolpoints'] = 2  # for B-spline free-form deformation？？？ what are the method details? By Chao.
 params['ModelParams']['sigma'] = 15 # used to produce randomly deformed images in data augmentation
+
 # params['ModelParams']['device'] = 0
 # params['ModelParams']['snapshot'] = 0
 params['ModelParams']['task'] = 'promise12'
+
 params['ModelParams']['dirTrainImage'] = os.path.join(basePath,'dataset/imagesTr')  # if 'dirTest' is empty, denotes 'path to a dataset that will later be split into trainSet and testSet. Otherwise, denotes just trainSet.
 params['ModelParams']['dirTrainLabel'] = os.path.join(basePath,'dataset/labelsTr')
 params['ModelParams']['dirTestImage'] = '' # path to test images
 params['ModelParams']['dirTestLabel'] = '' # path to test labels
+
+# added (uncommented) later
 # params['ModelParams']['testProp'] = 0.2  # if 'dirTestImage' or 'dirTestLabel' is empty, split 'dirTrainImage' and 'dirTrainLabel' into train and test
+
 params['ModelParams']['dirInferImage'] = os.path.join(basePath,'dataset/imagesTs') # used for inference, usually no labels provided.
 params['ModelParams']['dirResult'] = os.path.join(basePath,'results')  # where we need to save the results (relative to the base path)
+
+# seems like snapshots aren't saved while training, but still a model is saved - should be looked into -> when?
 # params['ModelParams']['dirSnapshots'] = os.path.join(basePath,'Models/MRI_cinque_snapshots/')  # where to save the models while training
+
 params['ModelParams']['nProc'] = 4  # the number of threads to do data augmentation
 
 
-#params of the DataManager
+# params of the DataManager
+
+# these two are passed separately as arguments below
 # params['DataManagerParams']['dstRes'] = np.asarray([1,1,1.5],dtype=float)
 # params['DataManagerParams']['VolSize'] = np.asarray([128, 128, 64],dtype=int)
-params['DataManagerParams']['normDir'] = False  # if rotates the volume according to its transformation in the mhd file. Not reccommended.
+
+params['DataManagerParams']['normDir'] = False  # it rotates the volume according to its transformation in the mhd file. Not reccommended.
 
 print('\n+preset parameters:\n' + str(params))
 
 
 #  parse sys.argv
 parser = argparse.ArgumentParser()
+
+# https://www.youtube.com/watch?v=QKCxAJsLWxQ
 parser.add_argument('--numcontrolpoints', type=int, default=2) # for B-spline free-form deformation？？？ what are the method details? By Chao.
+
 parser.add_argument('--testProp', type=float, default=0.2) # if 'dirTestImage' or 'dirTestLabel' is empty, split 'dirTrainImage' and 'dirTrainLabel' into train and test
+
 # this is output spacing
 parser.add_argument('--dstRes', type=str, default='[1, 1, 1.5]')
+
 # this is output size
 parser.add_argument('--VolSize', type=str, default='[128, 128, 64]')
 
 parser.add_argument('--batchsize', type=int, default=2)
 
-# around 30,000 - 50,000 for actual dataset
+# around 30,000 - 50,000 for actual dataset. After that it starts overfitting for promise12
 parser.add_argument('--numIterations', type=int, default=100) # the number of iterations, used by https://github.com/faustomilletari/VNet, as only one Epoch run.
 
 parser.add_argument('--baseLR', type=float, default=0.0001) # the learning rate, initial one
@@ -70,8 +85,11 @@ parser.add_argument('--nEpochs', type=int, default=1) # line "dataQueue_tmp = da
 parser.add_argument('--xLabel', type=str, default='Iteration', help='x-axis label for training performance transition curve, accepts "Epoch" or "Iteration"')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
+
+# if resuming training, pass path to stored checkpoint
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
                     help='path to latest checkpoint (default: none)')
+
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
 
